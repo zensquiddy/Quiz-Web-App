0620930ad5c89c1b9f9e55537f3dd89442add2b0
zensquiddy <zensquiddy@gmail.com>
2024-09-04 13:57:50 -0700

    Getting better

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
 	$("#p1").click(function(){
-		$("#p1").toggle();
-		$("h1").toggle();
-		$(".imgeveryone").toggle();
+		$("h1").text("Choose a Friend");
+		$(".imgeveryone").hide();
+		$("#p1").hide();
+		$("h2").hide();
 	});
+	$("#p2").click(function(){
+		$("h1").text("What is Evanders favorite word?");
+		$("#p2").html('<p onclick="myFunction()"> What? </p>');
+		$("#p4").html("<p> Crash Out </p>");
+		$("#p3").html("<p> You wanna die? </p>");
+		$("#p5").html("<p> No </p>");
+		$("#p6").hide();
+		
+	});
+	
+	$("#p3").click(function (){
+		$("h1").text("What is Pierces Favorite animal?");
+		$("#p2").html('<p id="pbees"> The Weird Bees </p>');
+		$("#p3").html("<p> The Deadly Rat </p>");
+		$("#p4").html("<p> The WereWolf</p>");
+		$("#p5").html("<p> Nettspend </p>");
+		$("#p6").hide();
+		
+	});
+	$("#pbees").click(function (){
+		$("h1").text("wrong");
+	});
+
+	
 });
+
+
+	function myFunction() {
+		$("h1").text("wrong");
+	}
